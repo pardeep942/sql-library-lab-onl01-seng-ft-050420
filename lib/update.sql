295fdcad164ae970226a407a63660a76df196739
PARDEEP KAUR <pkaur84@aol.com>
2020-06-02 06:34:42 +0000

    Automatically backed up by Learn

--- lib/update.sql
+++ lib/update.sql
@@ -1,3 +1,1 @@
- UPDATE [table name] SET [column name] = [new value] WHERE [column name] = [value];
-
-UPDATE characters SET species = "Martian" WHERE id = 8;
+ UPDATE Characters SET species = "Martian" WHERE species = "half-giant";
